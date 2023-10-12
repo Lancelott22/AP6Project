7971ef1c1ec56b88b5c8ba19fff1d5b00de1b4fb
Gebby <gerda.bagahansol@ctu.edu.ph>
2023-10-12 14:28:48 +0800

    a

--- ctuconnect/Interns/MyAccount.aspx.designer.cs
+++ ctuconnect/Interns/MyAccount.aspx.designer.cs
@@ -13,5 +13,23 @@ namespace ctuconnect
 
     public partial class MyAccount
     {
+
+        /// <summary>
+        /// disp_name control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label disp_name;
+
+        /// <summary>
+        /// disp_studentId control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label disp_studentId;
     }
 }
