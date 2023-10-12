d8cef57b1b0653328c64a5468ce6186b4a49c102
Gebby <gerda.bagahansol@ctu.edu.ph>
2023-10-12 14:23:00 +0800

    Login, register,  myAccount, IndustryHome pages

---
+++ ctuconnect/MyAccount.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ctuconnect
+{
+    public partial class MyAccount1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            disp_name.Text = Session["FNAME"].ToString() + " " + Session["INITIAL"].ToString()+ ". " + Session["LNAME"].ToString();
+            
+
+
+            
+        }
+    }
+}
