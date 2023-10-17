3db4b476159299efbed9d31b9be2e779b2cb4c2e
Lancelott <lancelott720@gmail.com>
2023-10-17 08:12:38 +0800

    b

--- ctuconnect/RegisterStudent.aspx.cs
+++ ctuconnect/RegisterStudent.aspx.cs
@@ -67,7 +67,9 @@ namespace ctuconnect
                 string filename = Path.GetFileName(postedFile.FileName); ///to check the filename
                 string fileExtension = Path.GetExtension(filename).ToLower(); //to get the extension filename
                 int filezise = postedFile.ContentLength; //to get the filesize
+
                 string logpath = "C:\\Users\\irish\\source\\repos\\ctuconnect\\ctuconnect\\images\\COR"; //creating a drive to upload or save the image
+
                 string filepath = Path.Combine(logpath, filename);
                 string fname = txtfname.Text;
                 string midinitial = txtinitial.Text;
@@ -81,7 +83,9 @@ namespace ctuconnect
                 string filename2 = Path.GetFileName(postedFile2.FileName);
                 string fileExtension2 = Path.GetExtension(filename2).ToLower();
                 int filezise2 = postedFile2.ContentLength;
-                string logpath2 = "C:\\Users\\Gebby\\source\\repos\\ctuconnect\\ctuconnect\\images\\StudentProfiles";
+
+                string logpath2 = "C:\\Users\\irish\\source\\repos\\ctuconnect\\ctuconnect\\images\\StudentProfiles";
+
                 string filepath2 = Path.Combine(logpath2, filename2);
 
             // Check if the email already exists in the database
