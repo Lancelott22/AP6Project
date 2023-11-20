5ee15bab46c200d35a9dad9d7b51f981f49473f2
Lance <lancelott720@gmail.com>
2023-11-20 20:02:27 +0800

    neewww

---
+++ ctuconnect/Admin_JobPosted.aspx.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Configuration;
+using System.Web.UI.HtmlControls;
+
+namespace ctuconnect
+{
+    public partial class Admin_JobPosted : System.Web.UI.Page
+    {
+        SqlConnection conDB = new SqlConnection(WebConfigurationManager.ConnectionStrings["CTUConnection"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            JobPostedBind();
+            TotalJob();
+        }
+
+        void JobPostedBind()
+        {
+            SqlCommand cmd = new SqlCommand("select * from HIRING JOIN INDUSTRY_ACCOUNT ON HIRING.industry_accID = INDUSTRY_ACCOUNT.industry_accID " +
+                "WHERE isActive = 'true' ORDER BY jobPostedDate DESC", conDB);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable ds = new DataTable();
+            da.Fill(ds);
+            JobPosted.DataSource = ds;
+            JobPosted.DataBind();
+            if (JobPosted.Items.Count == 0)
+            {
+                ListViewPager.Visible = false;
+            }
+        }
+        protected void JobPosted_ItemDataBound(object sender, ListViewItemEventArgs e)
+        {
+           
+            Label jobPostedDate = (Label)e.Item.FindControl("JobPostedDate");
+            ((Label)e.Item.FindControl("timeAgoMsg")).Text = postedDateTimeAgo(Convert.ToDateTime(jobPostedDate.Text));
+
+            DateTime postedDate = Convert.ToDateTime(jobPostedDate.Text);
+            DateTime currentDate = DateTime.Now;
+            TimeSpan timegap = currentDate - postedDate;
+
+            if (timegap.Days < 1)
+            {
+                HtmlGenericControl Badge = (HtmlGenericControl)e.Item.FindControl("badge");
+                Badge.Visible = true;
+                /* HtmlGenericControl JobList = (HtmlGenericControl)e.Item.FindControl("jobList");
+                 JobList.Style.Add("background-color", "#f0e789");*/
+            }
+        }
+
+        private string postedDateTimeAgo(DateTime postDateTime) //check the time gap between posted date and current date
+        {
+            string timeAgoMsg = "";
+            DateTime currentDate = DateTime.Now;
+            TimeSpan timegap = currentDate - postDateTime;
+            if (timegap.Days >= (365 * 2))
+            {
+                timeAgoMsg = string.Concat((((timegap.Days) / 30) / 12), " years ago");
+            }
+            else if (timegap.Days >= 365)
+            {
+                timeAgoMsg = string.Concat((((timegap.Days) / 30) / 12), " year ago");
+            }
+            else if (timegap.Days >= (30 * 2))
+            {
+                timeAgoMsg = string.Concat("About ", ((timegap.Days) / 30), " months ago");
+            }
+            else if (timegap.Days >= 31)
+            {
+                timeAgoMsg = string.Concat("About ", ((timegap.Days) / 30), " month ago");
+            }
+            else if (timegap.Days > 1)
+            {
+                timeAgoMsg = string.Concat(timegap.Days, " days ago");
+            }
+            else if (timegap.Days == 1)
+            {
+                timeAgoMsg = "Yesterday";
+            }
+            else if (timegap.Hours >= 2)
+            {
+                timeAgoMsg = string.Concat("About ", timegap.Hours, " hours ago");
+            }
+            else if (timegap.Hours >= 1)
+            {
+                timeAgoMsg = string.Concat("About ", timegap.Hours, " hour ago");
+            }
+            else if (timegap.Minutes > 1)
+            {
+                timeAgoMsg = string.Concat("About ", timegap.Minutes, " minutes ago");
+            }
+            else if (timegap.Minutes == 1)
+            {
+                timeAgoMsg = string.Concat("About ", timegap.Minutes, " minute ago");
+            }
+            else
+            {
+                timeAgoMsg = "Just Now";
+            }
+
+            return timeAgoMsg;
+        }
+        protected void JobPosted_PagePropertiesChanged(object sender, EventArgs e)
+        {
+            JobPostedBind();
+        }
+        void TotalJob()
+        {
+            
+            conDB.Open();
+            SqlCommand cmd = new SqlCommand("select COUNT(jobID) as TotalJob from HIRING WHERE isActive = 'true'", conDB);
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                totalJob.InnerText = "Total " + reader["TotalJob"].ToString() + " jobs found";
+            }
+            reader.Close();
+            conDB.Close();
+        }
+    }
+}
